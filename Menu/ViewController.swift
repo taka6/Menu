a13a0686349e8a4a054a7b13be185d4246b3935d
taka- <sugai.takashi@gmail.com>
2015-07-24 21:58:37 +0900

    Initial Commit

---
+++ Menu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Menu
+//
+//  Created by sugai.takashi on 2015/07/24.
+//  Copyright (c) 2015年 TAKASHI SUGAI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
